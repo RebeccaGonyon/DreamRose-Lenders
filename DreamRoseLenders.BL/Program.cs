8f4e3f64f89342e26aad56fa094808c4a8e762aa
Rebecca Gonyon <Rebecca752g@gmail.com>
2022-02-22 20:02:25 -0500

    added  more code to program.cs fileadded program.cs and states.cs files

---
+++ DreamRoseLenders.BL/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DreamRoseLenders.BL
+{
+    class Program : ProgramBase
+    {
+        public static void Main(string[] args, Lender fullName)
+        {
+            var lender = new Lender();
+            String FullName;
+            String CompanyName;
+            String Us_State;
+            {
+                Console.WriteLine("Please enter state you need loan in");
+                bool Name = string.IsNullOrWhiteSpace(args[0]);
+                bool US_State = Name;
+                Console.WriteLine("Lenders who loan in this state are:{CompanyName()}");
+                Console.WriteLine("The company contact is:{FullName()}");
+
+                Console.ReadLine();
+
+
+            }
+        }
+
+    }
+}
