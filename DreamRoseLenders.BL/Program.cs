35b0bc51ffeaec3c1be4e66c2ac2e73344d14dcb
Rebecca Gonyon <Rebecca752g@gmail.com>
2022-02-22 20:02:25 -0500

    added program.cs and states.cs files

---
+++ DreamRoseLenders.BL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamRoseLenders.BL
+{
+    class Program
+    {
+        Console.WriteLine("What state do you want your loan in?")
+    }
+}
